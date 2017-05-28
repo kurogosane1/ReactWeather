35f604d57a75703d70f0b44ba7392099fc6149b6
kurogosane1 <magnite@hotmail.com>
2017-05-28 15:50:51 -0500

    made some changes

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -9,7 +9,7 @@ var React =  require('react');
 // });
 var Examples = (props)=> {
    return (
-      <h3>Examples Components</h3>
+      <h3>Examples Components!</h3>
    )
 };
 module.exports= Examples;
