7022b4d9f6661d5361de7bcaf856ca9d37283647
kurogosane1 <magnite@hotmail.com>
2017-06-02 23:00:39 -0500

    made changes to the name

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -8,7 +8,7 @@ var Examples = (props) => {
       <p>Here are a few example locations to try out:</p>
       <ol>
         <li>
-          <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+          <Link to='/?location=Houston'>Houston,TX</Link>
         </li>
         <li>
           <Link to='/?location=Rio'>Rio, Brazil</Link>
